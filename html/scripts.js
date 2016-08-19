eb41db782d26025d190cbc234e2734da79236783
Gareth Ferneyhough <gareth@space.vu>
2016-08-19 09:59:59 -0700

    WIP

--- html/scripts.js
+++ html/scripts.js
@@ -1,22 +1,44 @@
-console.log('hi');
-function startNewGame() {
-    console.log('new game');
+var game_id = '';
 
-    // var myImage = document.querySelector('img');
-    // var responseElement = document.querySelector()
+function formatWordInPlay(wordInPlay) {
+    return wordInPlay.split('').join('  ');
+}
+
+function updatePage(gameState) {
+    var guessesRemainingElement = document.getElementById('guesses_remaining');
+    var gameBoardElement = document.getElementById('game_board');
+    guessesRemainingElement.innerHTML = 'Guesses Remaining: ' + gameState.guesses_remaining;
+    gameBoardElement.innerHTML = formatWordInPlay(gameState.state);
+    game_id = gameState.game_id;
+    console.log(gameState);
+}
+
+function startNewGame() {
     var myHeaders = new Headers();
     var myInit = { method: 'POST' };
 
     fetch('/game', myInit)
     .then(function(response) {
-        response.json().then(j => {
-            console.log(j);
+        response.json().then(json => {
+            updatePage(json);
         });
-        // console.log(response.json());
-      //return response.blob();
   });
-    // .then(function(myBlob) {
-    //   var objectURL = URL.createObjectURL(myBlob);
-    //   myImage.src = objectURL;
-    // });
+}
+
+function playTurn() {
+    var guess = document.getElementById('guess');
+    if (guess.value.length !== 1) {
+        alert('You must guess a single letter');
+        return;
+    }
+
+    var myHeaders = new Headers();
+    var myInit = { method: 'PATCH' };
+
+    fetch(`/${game_id}`, myInit)
+    .then(function(response) {
+        response.json().then(json => {
+            updatePage(json);
+        });
+  });
 }
